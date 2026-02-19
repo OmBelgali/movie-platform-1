1c0eab1ce0015a166f9cca0974e5a58e8de92280
Om Belgali <om.belgali@gmail.com>
2026-02-19 13:06:33 +0530

    style: revamp Row and RowItem for premium Netflix GUI

--- src/components/Row/RowItem.jsx
+++ src/components/Row/RowItem.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import styles from "./Row.module.css";
 
 function RowItem({ movie, isLarge }) {
-    const baseUrl = import.meta.env.VITE_TMDB_IMAGE_URL;
+    const baseUrl = "https://image.tmdb.org/t/p/";
+    const imageSize = isLarge ? "w500" : "w500"; // Using w500 for rows is standard
 
     return (
         <div className={`${styles.row_item} ${isLarge && styles.row_itemLarge}`}>
             <img
                 className={`${styles.row_poster} ${isLarge && styles.row_posterLarge}`}
-                src={`${baseUrl}${isLarge ? movie.poster_path : movie.backdrop_path
+                src={`${baseUrl}${imageSize}${isLarge ? movie.poster_path : movie.backdrop_path
                     }`}
                 alt={movie.name}
                 loading="lazy"
